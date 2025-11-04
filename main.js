51eefb072ca166ed8867fd96e32f5df29aff3297
LucasGabriel012 <241425105+developerlucasgabriel@users.noreply.github.com>
2025-11-04 00:50:21 -0300

    electron adicionado

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
-// Adicionando o recarregamento automático.
-require('electron-reload')(__dirname, {
-  electron: require(`${__dirname}/node_modules/electron`)
-});
-
-// Importando pacotes necessários para criar a janela da minha aplicação e controlar o ciclo de vida da aplicação.
 const { app, BrowserWindow } = require('electron');
+const electronReload = require('electron-reload');
+electronReload(__dirname);
 
-// Criando minha função para criar a jenela e definindo algumas caracteristicas de como eu quero ela.
+
 const createWindow = () => {
-    
+
     const win = new BrowserWindow({
-        width: 1000,
-        height: 700,
-        minWidth: 800,
-        minHeight: 600,
-        backgroundColor: '#f4f4f4',
+      width: 1200,
+      maxWidth: 1200,
+      height: 800,
+      maxHeight: 800,
+      backgroundColor: "lighslategray"
     });
 
-    // Carregando arquivo html. 
     win.loadFile('index.html');
+
 };
 
-// Só inicia a aplicação após todo o electron for carregado.
+
 app.whenReady().then(() => {
-createWindow();
+    createWindow();
 });
